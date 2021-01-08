127c2b9371f05847bb16cbb8277b2b7376dfe2f4
rvdw_vbook <rogerscode@gmail.com>
2021-01-08 23:59:18 +0100

    Seed argument settiing included and test jobfile created

--- Transparency/train_and_run_experiments_bc.py
+++ Transparency/train_and_run_experiments_bc.py
@@ -6,12 +6,25 @@ parser.add_argument("--output_dir", type=str)
 parser.add_argument('--encoder', type=str, choices=['vanilla_lstm', 'ortho_lstm', 'diversity_lstm'], required=True)
 parser.add_argument('--attention', type=str, choices=['tanh', 'dot', 'all'], default='tanh') # TODO: does attention work with dot/all? what are they?
 parser.add_argument("--diversity",type=float,default=0)
+parser.add_argument("--seed", type=int,default=0)
 
 args, extras = parser.parse_known_args()
 args.extras = extras
 
 from Transparency.Trainers.DatasetBC import *
 from Transparency.ExperimentsBC import *
+
+import random
+random.seed(args.seed)
+np.random.seed(args.seed)
+torch.manual_seed(args.seed)
+if torch.cuda.is_available():
+	#torch.set_deterministic(True)
+	torch.cuda.manual_seed_all(args.seed)
+	#torch.backends.cudnn.deterministic = True
+	#torch.backends.cudnn.benchmark = False
+print('NEWJOB')
+print('seed ',args.seed,', dataset ',args.dataset,', model ',args.encoder,', diversity ',args.diversity)
 
 dataset = datasets[args.dataset](args)
 
