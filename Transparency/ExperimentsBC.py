1a83e5308951603093cd94d5b7b16cd64af4743e
yun <liyun.elf@gmail.com>
2021-01-12 14:05:57 +0100

    ran linter (black) for better code formatting

--- Transparency/ExperimentsBC.py
+++ Transparency/ExperimentsBC.py
@@ -3,71 +3,96 @@ from Transparency.Trainers.PlottingBC import generate_graphs
 from Transparency.configurations import configurations
 from Transparency.Trainers.TrainerBC import Trainer, Evaluator, RationaleTrainer
 
-def train_dataset(dataset, config='lstm') :
-    print('STARTING TRAINING')
+
+def train_dataset(dataset, config="lstm"):
+    print("STARTING TRAINING")
 
     config = configurations[config](dataset)
     trainer = Trainer(dataset, config=config, _type=dataset.trainer_type)
-    if hasattr(dataset,'n_iter'):
+    if hasattr(dataset, "n_iter"):
         n_iters = dataset.n_iter
     else:
         n_iters = 8
-    
-    trainer.train(dataset.train_data, dataset.dev_data, n_iters=n_iters, save_on_metric=dataset.save_on_metric)
+
+    trainer.train(
+        dataset.train_data,
+        dataset.dev_data,
+        n_iters=n_iters,
+        save_on_metric=dataset.save_on_metric,
+    )
     evaluator = Evaluator(dataset, trainer.model.dirname, _type=dataset.trainer_type)
     _ = evaluator.evaluate(dataset.test_data, save_results=True)
     return trainer, evaluator
 
-def train_dataset_on_encoders(dataset, encoders) :
-    for e in encoders :
+
+def train_dataset_on_encoders(dataset, encoders):
+    for e in encoders:
         train_dataset(dataset, e)
         run_experiments_on_latest_model(dataset, e)
         run_rationale_on_latest_model(dataset, e)
-        
-def generate_graphs_on_encoders(dataset, encoders) :
-    for e in encoders :
+
+
+def generate_graphs_on_encoders(dataset, encoders):
+    for e in encoders:
         generate_graphs_on_latest_model(dataset, e)
 
-def run_rationale_on_latest_model(dataset, config='lstm') :
+
+def run_rationale_on_latest_model(dataset, config="lstm"):
     config = configurations[config](dataset)
-    latest_model = get_latest_model(os.path.join(config['training']['basepath'], config['training']['exp_dirname']))
-    rationale_gen = RationaleTrainer(dataset, config, latest_model, _type=dataset.trainer_type)
-    print ('Training the Rationale Generator ...')
-    _ = rationale_gen.train(dataset.train_data,dataset.dev_data)
-    print ('Running Exp to Compute Attention given to Rationales ...')
+    latest_model = get_latest_model(
+        os.path.join(config["training"]["basepath"], config["training"]["exp_dirname"])
+    )
+    rationale_gen = RationaleTrainer(
+        dataset, config, latest_model, _type=dataset.trainer_type
+    )
+    print("Training the Rationale Generator ...")
+    _ = rationale_gen.train(dataset.train_data, dataset.dev_data)
+    print("Running Exp to Compute Attention given to Rationales ...")
     rationale_gen.rationale_attn_experiment(dataset.test_data)
     return rationale_gen
 
-def run_evaluator_on_latest_model(dataset, config='lstm') :
-    print('EVALUATING LATEST MODEL')
+
+def run_evaluator_on_latest_model(dataset, config="lstm"):
+    print("EVALUATING LATEST MODEL")
 
     config = configurations[config](dataset)
-    latest_model = get_latest_model(os.path.join(config['training']['basepath'], config['training']['exp_dirname']))
+    latest_model = get_latest_model(
+        os.path.join(config["training"]["basepath"], config["training"]["exp_dirname"])
+    )
     evaluator = Evaluator(dataset, latest_model, _type=dataset.trainer_type)
     _ = evaluator.evaluate(dataset.test_data, save_results=True)
     return evaluator
 
-def run_experiments_on_latest_model(dataset, config='lstm', force_run=True) :
-        evaluator = run_evaluator_on_latest_model(dataset, config)
-        test_data = dataset.test_data
-        print('RUNNING GRADIENT EXPERIMENT ON LATEST MODEL')
-        evaluator.gradient_experiment(test_data, force_run=force_run)
-        print('RUNNING QUANTITATIVE ANALYSIS EXPERIMENT ON LATEST MODEL')
-        evaluator.quantitative_analysis_experiment(test_data, dataset, force_run=force_run)
-        print('RUNNING IMPORTANCE RANKING EXPERIMENT ON LATEST MODEL')
-        evaluator.importance_ranking_experiment(test_data, force_run=force_run)
-        print('RUNNING CONICITY ANALYSIS EXPERIMENT ON LATEST MODEL')
-        evaluator.conicity_analysis_experiment(test_data)
-        print('RUNNING PERMUTATION EXPERIMENT ON LATEST MODEL')
-        evaluator.permutation_experiment(test_data, force_run=force_run)
-        print('RUNNING INTEGRATED GRADIENT EXPERIMENT ON LATEST MODEL')
-        evaluator.integrated_gradient_experiment(dataset, force_run=force_run)
 
-def generate_graphs_on_latest_model(dataset, config='lstm'):
-    print('GENERATING GRAPHS FOR EXPERIMENT ON LATEST MODEL')
+def run_experiments_on_latest_model(dataset, config="lstm", force_run=True):
+    evaluator = run_evaluator_on_latest_model(dataset, config)
+    test_data = dataset.test_data
+    print("RUNNING GRADIENT EXPERIMENT ON LATEST MODEL")
+    evaluator.gradient_experiment(test_data, force_run=force_run)
+    print("RUNNING QUANTITATIVE ANALYSIS EXPERIMENT ON LATEST MODEL")
+    evaluator.quantitative_analysis_experiment(test_data, dataset, force_run=force_run)
+    print("RUNNING IMPORTANCE RANKING EXPERIMENT ON LATEST MODEL")
+    evaluator.importance_ranking_experiment(test_data, force_run=force_run)
+    print("RUNNING CONICITY ANALYSIS EXPERIMENT ON LATEST MODEL")
+    evaluator.conicity_analysis_experiment(test_data)
+    print("RUNNING PERMUTATION EXPERIMENT ON LATEST MODEL")
+    evaluator.permutation_experiment(test_data, force_run=force_run)
+    print("RUNNING INTEGRATED GRADIENT EXPERIMENT ON LATEST MODEL")
+    evaluator.integrated_gradient_experiment(dataset, force_run=force_run)
+
+
+def generate_graphs_on_latest_model(dataset, config="lstm"):
+    print("GENERATING GRAPHS FOR EXPERIMENT ON LATEST MODEL")
 
     config = configurations[config](dataset)
-    latest_model = get_latest_model(os.path.join(config['training']['basepath'], config['training']['exp_dirname']))
+    latest_model = get_latest_model(
+        os.path.join(config["training"]["basepath"], config["training"]["exp_dirname"])
+    )
     evaluator = Evaluator(dataset, latest_model, _type=dataset.trainer_type)
     _ = evaluator.evaluate(dataset.test_data, save_results=False)
-    generate_graphs(dataset, config['training']['exp_dirname'], evaluator.model, test_data=dataset.test_data)
+    generate_graphs(
+        dataset,
+        config["training"]["exp_dirname"],
+        evaluator.model,
+        test_data=dataset.test_data,
+    )
--- Transparency/train_and_run_experiments_bc.py
+++ Transparency/train_and_run_experiments_bc.py
@@ -1,13 +1,23 @@
 import argparse
-parser = argparse.ArgumentParser(description='Run experiments on a dataset')
-parser.add_argument('--dataset', type=str, required=True)
+
+parser = argparse.ArgumentParser(description="Run experiments on a dataset")
+parser.add_argument("--dataset", type=str, required=True)
 parser.add_argument("--data_dir", type=str, required=True)
 parser.add_argument("--output_dir", type=str)
-parser.add_argument('--encoder', type=str, choices=['vanilla_lstm', 'ortho_lstm', 'diversity_lstm'], required=True)
-parser.add_argument('--attention', type=str, choices=['tanh', 'dot', 'all'], default='tanh') # TODO: does attention work with dot/all? what are they?
-parser.add_argument("--diversity",type=float,default=0)
-parser.add_argument("--seed", type=int,default=0)
-parser.add_argument('--job_type', type=str, default='both', choices=['both', 'train', 'experiment'])
+parser.add_argument(
+    "--encoder",
+    type=str,
+    choices=["vanilla_lstm", "ortho_lstm", "diversity_lstm"],
+    required=True,
+)
+parser.add_argument(
+    "--attention", type=str, choices=["tanh", "dot", "all"], default="tanh"
+)  # TODO: does attention work with dot/all? what are they?
+parser.add_argument("--diversity", type=float, default=0)
+parser.add_argument("--seed", type=int, default=0)
+parser.add_argument(
+    "--job_type", type=str, default="both", choices=["both", "train", "experiment"]
+)
 
 args, extras = parser.parse_known_args()
 args.extras = extras
@@ -16,35 +26,50 @@ import random
 import numpy as np
 import torch
 from Transparency.Trainers.DatasetBC import datasets
-from Transparency.ExperimentsBC import train_dataset, train_dataset_on_encoders, generate_graphs_on_encoders, run_experiments_on_latest_model, run_rationale_on_latest_model
+from Transparency.ExperimentsBC import (
+    train_dataset,
+    train_dataset_on_encoders,
+    generate_graphs_on_encoders,
+    run_experiments_on_latest_model,
+    run_rationale_on_latest_model,
+)
 
 random.seed(args.seed)
 np.random.seed(args.seed)
 torch.manual_seed(args.seed)
 if torch.cuda.is_available():
-	#torch.set_deterministic(True)
-	torch.cuda.manual_seed_all(args.seed)
-	#torch.backends.cudnn.deterministic = True
-	#torch.backends.cudnn.benchmark = False
+    # torch.set_deterministic(True)
+    torch.cuda.manual_seed_all(args.seed)
+    # torch.backends.cudnn.deterministic = True
+    # torch.backends.cudnn.benchmark = False
 
-print('NEWJOB')
-print('seed ',args.seed,', dataset ',args.dataset,', model ',args.encoder,', diversity ',args.diversity)
+print("NEWJOB")
+print(
+    "seed ",
+    args.seed,
+    ", dataset ",
+    args.dataset,
+    ", model ",
+    args.encoder,
+    ", diversity ",
+    args.diversity,
+)
 
 dataset = datasets[args.dataset](args)
 
-if args.output_dir is not None :
+if args.output_dir is not None:
     dataset.output_dir = args.output_dir
 
 dataset.diversity = args.diversity
 encoders = [args.encoder]
 
-if args.job_type == 'both':
+if args.job_type == "both":
     train_dataset_on_encoders(dataset, encoders)
     generate_graphs_on_encoders(dataset, encoders)
-elif args.job_type == 'train':
+elif args.job_type == "train":
     # only train the (ortho/diverse)lstm+attention model, without other experiments
     train_dataset(dataset, args.encoder)
-elif args.job_type == 'experiment':
+elif args.job_type == "experiment":
     # only run experiments using the latest trained model
     run_experiments_on_latest_model(dataset, args.encoder)
     run_rationale_on_latest_model(dataset, args.encoder)
