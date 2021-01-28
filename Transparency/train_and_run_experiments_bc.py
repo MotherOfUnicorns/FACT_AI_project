808ddba42779328b0dea921b04f55ac15e926516
yun <11966862+MotherOfUnicorns@users.noreply.github.com>
2021-01-28 22:01:45 +0100

    Update train_and_run_experiments_bc.py

--- Transparency/train_and_run_experiments_bc.py
+++ Transparency/train_and_run_experiments_bc.py
@@ -11,8 +11,8 @@ parser.add_argument(
     required=True,
 )
 parser.add_argument(
-    "--attention", type=str, choices=["tanh", "equal", "first_only", "last_only","dot"], default="tanh"
-)  # TODO: does attention work with dot/all? what are they?
+    "--attention", type=str, choices=["tanh", "equal", "first_only", "last_only"], default="tanh"
+) 
 parser.add_argument("--diversity", type=float, default=0)
 parser.add_argument("--seed", type=int, default=0)
 parser.add_argument(
